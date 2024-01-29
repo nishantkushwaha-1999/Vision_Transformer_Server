d47984ef8a3004b1f31b7ac8cae236ed8fd898ae
mostwanted-786 <60353780+nishantkushwaha-1999@users.noreply.github.com>
2024-01-29 12:52:50 -0600

    changes

--- mnist_all.py
+++ mnist_all.py
@@ -7,14 +7,14 @@ mn = mnist.MNIST()
 mn.load_model("basicCNN/basicCNN")
 
 @anvil.server.callable
-def evaluate():
-    result = mn.evaluate()
-    return result
-
 def history_all():
     with open("basicCNN/history_val.json", 'r') as fp:
         data = json.load(fp)
     return data
+
+def evaluate():
+    result = mn.evaluate()
+    return result
 
 anvil.server.connect("server_O5OPT6KC6T6WXPJUMOM7IJUG-VJ5KJZ4SWIN2DSBR")
 anvil.server.wait_forever()
