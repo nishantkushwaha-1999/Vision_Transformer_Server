6152a66ce238b9f69d24b55e7dc364a42e34862b
mostwanted-786 <60353780+nishantkushwaha-1999@users.noreply.github.com>
2024-02-09 21:06:23 -0600

    .

--- basicCNN/mnistHandler.py
+++ basicCNN/mnistHandler.py
@@ -20,13 +20,13 @@ lyrs = [('convo2d', {'filters':15, 'kernel_size':(2, 2), 'strides':(1, 1), 'acti
         ('dense', {'units':256, 'activation':'relu'}),
         ('dense', {'units':128, 'activation':'relu'})]
 
-mn.initializeConvNetwork(layers=lyrs)
-mn.compile(0.001)
-history_val = mn.fit(batch_size=128, validation_split=0.2, epochs=20)
+# mn.initializeConvNetwork(layers=lyrs)
+# mn.compile(0.001)
+# history_val = mn.fit(batch_size=128, validation_split=0.2, epochs=20)
 
-with open("history_val.json", "w") as fp:
-	json.dump(history_val.history, fp)
-	# fp.writelines(str(history_val.history))
+# with open("history_val.json", "w") as fp:
+# 	json.dump(history_val.history, fp)
+# 	# fp.writelines(str(history_val.history))
 
 mn.initializeConvNetwork(layers=lyrs)
 mn.compile(0.001)
