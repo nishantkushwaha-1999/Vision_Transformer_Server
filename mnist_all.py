6bf631b97fc95c05cc63939bd0aba796baeac7f4
mostwanted-786 <60353780+nishantkushwaha-1999@users.noreply.github.com>
2024-02-12 14:35:03 -0600

    beta test

--- mnist_all.py
+++ mnist_all.py
@@ -36,82 +36,54 @@ class Handler():
   
   def convert_image(self, file):
     try:
-      self.file = file.get_bytes()
+      file = file.get_bytes()
       im_df = pd.read_csv(BytesIO(self.file), header=None)
-      im_df = np.array(im_df)
+      self.im_df = np.array(im_df)
 
       if im_df.shape != (28, 28):
-        raise ValueError(f"Expected file of shape(28, 28), but recieved with shape {im_df.shape}")
+        raise ValueError(f"Expected file of shape(28, 28), but recieved with shape {self.im_df.shape}")
       
-      image = Image.fromarray(im_df, '1')
+      image = Image.fromarray(self.im_df)
+      image = image.convert("L")
       bs = BytesIO()
       image.save(bs, format='JPEG')
 
       return anvil.BlobMedia("image/jpeg", bs.getvalue(), name='input')
     except Error as e:
       return e
-       
-h = Handler()
-print(h.evaluate('vit'))
+  
+  def predict(self, model: str):
+    try:
+      if model=='basicCNN':
+        val = self.mn.predict(self.im_df)
+      elif model=='vit':
+        df = np.expand_dims(self.im_df, axis=0)
+        df = self.vit.preprocess_data(df)[0]
+        val = self.vit.predict(df)
+      else:
+        raise ValueError(f"No model found with name {model}")
+      return val
+    except Error as e:
+      return e
+
+
+handler = Handler()
+
+@anvil.server.callable
+def get_history(model, type):
+  return handler.get_history(model, type)
+
+@anvil.server.callable
+def evaluate(model):
+  return handler.evaluate(model)
 
 @anvil.server.callable
 def image(file):
-  return h.convert_image(file)
+  return handler.convert_image(file)
 
-# @anvil.server.callable
-# def get_history(model: str, type: str):
-#   path = f"{model}/history_{type}.json"
-#   with open("basicCNN/history_val.json", 'r') as fp:
-#       data = json.load(fp)
-#   return data
-
-# @anvil.server.callable
-# def evaluate_bCNN():
-#     result = mn.evaluate()
-#     return result
-
-# @anvil.server.callable
-# def image(file):
-#    try:
-#       file = file.get_bytes()
-#       file = pd.read_csv(BytesIO(file), header=None)
-#       df = np.array(df)
-      
-#       val = mn.predict(df)
-#       return val
-    
-#     except Error as e:
-#       return e
-   
-
-# @anvil.server.callable
-# def predict_basicCNN(file):
-#     try:
-#       file = file.get_bytes()
-#       df = pd.read_csv(BytesIO(file), header=None)
-#       df = np.array(df)
-      
-#       val = mn.predict(df)
-#       return val
-    
-#     except Error as e:
-#       return e
-
-# @anvil.server.callable
-# def predict_vit(file):
-#     try:
-#       file = file.get_bytes()
-#       df = pd.read_csv(BytesIO(file), header=None)
-#       df = np.array(df)
-#       df = np.expand_dims(df, axis=0)
-#       df = vit.preprocess_data(df)[0]
-#       print(df.shape)
-      
-#       val = vit.predict(df)
-#       return val
-    
-#     except Error as e:
-#       return e
+@anvil.server.callable
+def image(model):
+  return handler.predict(model)
 
 anvil.server.connect("server_O5OPT6KC6T6WXPJUMOM7IJUG-VJ5KJZ4SWIN2DSBR")
 anvil.server.wait_forever()
